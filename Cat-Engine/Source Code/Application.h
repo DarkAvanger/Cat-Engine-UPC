05030ad31bbff0df8d241b30b329e2feb868a4ba
DarkAvanger <awesomekoala19@gmail.com>
2022-09-26 19:29:16 +0200

    STL

--- Cat-Engine/Source Code/Application.h
+++ Cat-Engine/Source Code/Application.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "p2List.h"
 #include "Globals.h"
 #include "Timer.h"
 #include "Module.h"
@@ -9,6 +8,7 @@
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
 #include "ModuleEditor.h"
+#include <list>
 
 
 class Application
@@ -24,7 +24,7 @@ private:
 
 	Timer	ms_timer;
 	float	dt;
-	p2List<Module*> list_modules;
+	std::list<Module*> list_modules;
 
 public:
 
