843ade7ad301ab4dcecf8bb876242bab33a7224a
DarkAvanger <awesomekoala19@gmail.com>
2022-11-01 20:08:32 +0100

    Pyramide

--- Cat Engine/Source Code/Primitives.h
+++ Cat Engine/Source Code/Primitives.h
@@ -62,7 +62,38 @@ namespace RPyramide
 {
 	void CreatePyramide(std::vector<float3>& vertices, std::vector<unsigned int>& indices, std::vector<float2>& texCoords)
 	{
-		
+		vertices =
+		{
+			{-0.5f, -0.5f, 0.5f},
+			{0.5f, -0.5f, 0.5f},
+			{-0.5f, -0.5f, -0.5f},
+			{0.5f, -0.5f, -0.5f},
+			{0.0f, 0.5f, 0.0f},
+		};
+
+		indices =
+		{
+			1,0,2,
+
+			2,3,1,
+
+			0,1,4,
+
+			1,3,4,
+
+			3,2,4,
+
+			2,0,4
+		};
+
+		texCoords =
+		{
+			{1.0f, 0.0f},
+			{0.0f, 0.0f},
+			{0.0f, 0.0f},
+			{1.0f, 0.0f},
+			{0.5f, 1.0f},
+		};
 	}
 }
 
