1f4d0acd82d7fd01d9872490afbd707eaa789267
DarkAvanger <awesomekoala19@gmail.com>
2022-11-01 20:08:10 +0100

    Cube

--- Cat Engine/Source Code/Primitives.h
+++ Cat Engine/Source Code/Primitives.h
@@ -9,7 +9,52 @@ namespace RCube
 {
 	void CreateCube(std::vector<float3>& vertices, std::vector<unsigned int>& indices, std::vector<float2>& texCoords)
 	{
+		vertices =
+		{
+			{-0.5f, 0.5f, 0.5f},
+			{-0.5f, -0.5f, 0.5f},
+			{0.5f, -0.5f, 0.5f},
+			{0.5f, 0.5f, 0.5f},
 
+			{0.5f, -0.5f, -0.5f},
+			{0.5f, 0.5f, -0.5f},
+			{-0.5f, -0.5f, -0.5f},
+			{-0.5f, 0.5f, -0.5f},
+		};
+
+		indices =
+		{
+			0,1,2,
+			2,3,0,
+
+			3,2,4,
+			4,5,3,
+
+			5,4,6,
+			6,7,5,
+
+			7,6,1,
+			1,0,7,
+
+			3,5,7,
+			7,0,3,
+
+			1,6,4,
+			4,2,1
+		};
+
+		texCoords =
+		{
+			{0.0f, 1.0f},
+			{0.0f, 0.0f},
+			{1.0f, 0.0f},
+			{1.0f, 1.0f},
+
+			{1.0f, 1.0f},
+			{1.0f, 0.0f},
+			{0.0f, 1.0f},
+			{0.0f, 0.0f},
+		};
 	}
 }
 
