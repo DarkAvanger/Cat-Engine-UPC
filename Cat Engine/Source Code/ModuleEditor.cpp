11330322525b6aba5bc169a7d356a07bfe69508d
DarkAvanger <awesomekoala19@gmail.com>
2022-12-04 21:03:34 +0100

    Last minute

--- Cat Engine/Source Code/ModuleEditor.cpp
+++ Cat Engine/Source Code/ModuleEditor.cpp
@@ -63,7 +63,7 @@ bool ModuleEditor::Update(float dt)
 	if (app->input->GetKey(SDL_SCANCODE_LCTRL) == KeyState::KEY_REPEAT &&
 		app->input->GetKey(SDL_SCANCODE_D) == KeyState::KEY_DOWN)
 	{
-		if (selected) app->scene->DuplicateGO(selected, selectedParent);
+		//if (selected) app->scene->DuplicateGO(selected, selectedParent);
 	}
 
 	return ret;
